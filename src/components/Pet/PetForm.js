32ad5df79e8df942a31e5df033aa67cf04c63845
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-05 09:57:33 -0600

    make pet form dynamic with state

--- src/components/Pet/PetForm.js
+++ src/components/Pet/PetForm.js
@@ -1,8 +1,20 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-function PetForm(update) {
-  const { updateFunction } = update;
+function PetForm(props) {
+  const { formData, updateFunction } = props;
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    updateFunction({...formData, [name]: value})
+  };
+
+  const handleCheckBoxChange = (e) => {
+    const checked = e.target.checked;
+    const name = e.target.name
+    updateFunction({...formData, [name]: checked})
+  };
 
   return (
     <>
@@ -10,30 +22,60 @@ function PetForm(update) {
       <Form>
         <Form.Group className="mb-3" controlId="formSpecies">
           <Form.Label>Species</Form.Label>
-          <Form.Control type="text" placeholder="Species of the pet" />
+          <Form.Control
+            name="species"
+            type="text"
+            placeholder="Species of the pet"
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formColor">
           <Form.Label>Color</Form.Label>
-          <Form.Control type="text" placeholder="A brief description of its color" />
+          <Form.Control
+            name="color"
+            type="text"
+            placeholder="A brief description of its color"
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formWeight">
           <Form.Label>Weight</Form.Label>
-          <Form.Control type="text" placeholder="The pet's weight (lbs)" />
+          <Form.Control
+            name="weight"
+            type="text"
+            placeholder="The pet's weight (lbs)"
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formIsHousebroken">
           <Form.Label>House Training</Form.Label>
-          <Form.Check type="text" label="Is the pet housebroken?" />
+          <Form.Check
+            name="isHousebroken"
+            type="checkbox"
+            label="Is the pet housebroken?"
+            onChange={handleCheckBoxChange}
+          />
         </Form.Group>
         <Form.Group controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name of Pet"/>
+          <Form.Control
+            name="name"
+            type="text"
+            placeholder="Name of Pet"
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group controlId="formPersonality">
           <Form.Label>Personality</Form.Label>
-          <Form.Control as="textarea" rows={3}/>
+          <Form.Control
+            name="personality"
+            as="textarea"
+            rows={3}
+            onChange={handleChange}
+          />
           <Form.Text className="text-muted">
-            Please input a description of the pet's personality, habits,
-            and how they get along with people, kids, and other pets.
+            Please input a description of the pet's personality, habits, and how
+            they get along with people, kids, and other pets.
           </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
