bd8d353ff6a8743b99ebf7c7b7f1e8ccf667ddea
Nick Landas <nick.landas@gmail.com>
2023-01-04 18:45:11 -0600

    Added footer detail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
     <div className="text-center bg-dark text-white py-2 fixed-bottom">
-        <span>Developed by  &copy; {new Date().getFullYear()}</span>
+        <span>Developed by Rimon-Nick &copy; {new Date().getFullYear()}</span>
     </div>
     
     </>
