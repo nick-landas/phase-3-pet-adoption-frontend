a78c4d7f035d3ac02ad00f65eb7ae20c3bfc0a31
Nick Landas <nick.landas@gmail.com>
2023-01-04 18:41:19 -0600

    Added footer detail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  );
+    <>
+    <div className="text-center bg-dark text-white py-2 fixed-bottom">
+        <span>Developed by  &copy; {new Date().getFullYear()}</span>
+    </div>
+    
+    </>
+  )
 }
+
 
 export default Footer;
